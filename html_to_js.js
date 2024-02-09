ca5699bd78c6db7d8018dc44a8b757c35ac1e96f
Amurharsha07 <150145508+Amurharsha07@users.noreply.github.com>
2024-02-09 10:16:39 +0530

    Add files via upload
    Noob.js

---
+++ html_to_js.js
@@ -0,0 +1,223 @@
+let jsValue = `
+<html>
+
+<head>
+
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/howler/2.0.3/howler.min.js">
+
+</script>
+
+
+
+<title>
+HACKED BY  CYBER GIRL 
+</title>
+
+    
+<meta charset="UTF-8">
+
+        
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+
+<meta property="og:image" content="https://i.ibb.co/7gRDqsh/Picsart-24-01-24-00-22-21-844-removebg-preview.png">
+
+
+<link rel="SHORTCUT ICON" href=" ">
+
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+
+
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+
+
+<link href="https://fonts.googleapis.com/css2?family=Bebas Neue&display=swap" rel="stylesheet">
+
+
+<link href="https://fonts.googleapis.com/css2?family=Bebas Neue&family=Josefin Sans:wght@400;500&family=Righteous&display=swap" rel="stylesheet">
+
+
+<link href="https://fonts.googleapis.com/css2?family=Bebas Neue&family=Righteous&display=swap" rel="stylesheet">
+
+
+<link href="https://fonts.googleapis.com/css2?family=Titillium Web:wght@400;600&display=swap" rel="stylesheet">
+
+
+
+
+
+
+</head>
+ 
+<body>
+
+
+<style type="text/css">
+
+	body{
+		background-image: linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url('https://i.ibb.co/JCPtxg5/images-9-2.jpg');    
+        background-position: center;
+		background-size: cover;
+			margin: 0 auto;
+			position: relative;
+	}
+.message-box{
+		    font-family: 'Josefin Sans', cursive;
+		    color: white;
+		    text-align: center;
+		    text-shadow: 0 0 10px red;
+		}
+		
+      
+
+
+
+
+	  
+</style>
+
+
+    
+<script type="text/javascript" src="http://aakashbapna.github.io/jet-planes/doodle.js">
+
+</script>
+
+
+<script type="text/javascript">
+
+doodle.init("http://aakashbapna.github.io/jet-planes/jet.png");
+
+</script>
+
+
+<div align="center">
+
+<img style="padding:50; height: 300; width: 300px;" align="center" src="https://i.ibb.co/7gRDqsh/Picsart-24-01-24-00-22-21-844-removebg-preview.png" alt="resim">
+<style> img{ animation-name: rotate ; animation-duration: 6s; animation-play-state: running; animation-timing-function: linear; animation-iteration-count: infinite; } @keyframes rotate{ 10% {transform:rotateY(36deg)} 20% {transform:rotateY(72deg)} 30% {transform:rotateY(108deg)} 40% {transform:rotateY(144deg)} 50% {transform:rotateY(180deg)} 60% {transform:rotateY(216deg)} 70% {transform:rotateY(252deg)} 80% {transform:rotateY(288deg)} 90% {transform:rotateY(324deg)} 100% {transform:rotateY(360deg)} } 
+    </style>
+
+</div>
+
+
+<div class="main-box">
+
+
+<center>
+
+
+<font color="orange"size="9"style="text-shadow: 0px 3px 12px orange">HACKED</font>
+<font color="white"size="9"style="text-shadow: 0px 3px 12px white">BY</font>
+<font color="lime"size="9"style="text-shadoow:0px 3px 12px lime ">CYBER GIRL</font>
+			
+<br>
+
+
+<br>
+
+
+<br>
+
+		
+<h1 class="logo-wish">
+
+			
+<font size="6" color="gold">
+BLACK DAY 
+</font>
+<br><br>
+<font color="red" size="6">HELLO ADMIN YOU HAVE BEEN HACKED</font>
+
+
+		
+</h1>
+
+
+					
+			
+<div style="padding-top: 35px;">
+##<br><br><br><br>
+			
+<center>
+
+<font color="lime">
+  <font color="red"size="19"style="text-shadow: 0px 3px 12px black">Message: ></font>
+
+
+
+			
+<center>
+  <br><br><br><br><br><br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+
+<font color="orange"size ="6">
+ पुलवामा आतंकी हमले में शहीद हुए सभी जवानों को हुए सभी जवानों को भावपूर्ण श्रद्धांजलि</font>
+
+</font>
+<br><br><Br><br><br>
+			
+</center>
+
+</center>
+
+</div>
+
+		
+      
+	  
+<div class="footer-greetings">
+
+<marquee behavior="loop">
+
+<font color="lime" face="iceland" size="4">
+
+		Greetz : TEAM UCC | |DARK CYBER WARRIOR| | 𝐓𝐄𝐀𝐌 𝐍𝐖𝐇 𝐒𝐄𝐂𝐔𝐑𝐈𝐓𝐘 | |𝐀𝐍𝐎𝐍𝐘𝐌𝐎𝐔𝐒 𝐈𝐍𝐃𝐈𝐀 | |Team Network Nine | |Unknown Cyber Cult | |Team BlackDragonSec | |𝗜𝗻𝗱𝗶𝗮𝗻𝗖𝘆𝗯𝗲𝗿𝗣𝘂𝗻𝗸 | |INDIAN CYBER MAFIA | |Indian Cyber Force | | ANON SEC | | DemonSec|
+
+ <body>
+
+ </body>
+
+</iframe>
+
+
+
+</font>
+
+
+
+
+</marquee>
+
+</div>
+
+</center>
+
+</center>
+
+</center>
+
+</div>
+
+</center>
+
+</div>
+
+</body>
+
+<body><center><audio auto-pay="1" controls="controls" src="https://j.top4top.io/m_2960k6o6w4.mp3"></audio></center> 			</body></font></font> 			<br> 			</div> 		 </div> 		</div> 	 </div> 	</div>
+
+</body>
+
+</html>`;
