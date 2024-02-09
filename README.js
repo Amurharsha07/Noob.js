173d72ecb4d3578ce4b6a18b531c5e82770ddfc1
Amurharsha07 <150145508+Amurharsha07@users.noreply.github.com>
2024-02-09 10:25:13 +0530

    Create README.html

---
+++ README.js
@@ -0,0 +1,135 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=New+Rocker" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Iceland">
+    <link rel="icon" type="image/jpg" href="https://i.ibb.co/7gRDqsh/Picsart-24-01-24-00-22-21-844-removebg-preview.png" />
+    <meta name="description" content="noobdi">
+    <meta property="og:image" content="https://i.ibb.co/fkGnyDJ/dcw.png">
+    <meta property="og:image:url" content="https://i.ibb.co/fkGnyDJ/dcw.png">
+    <meta property="og:image:secure_url" content="https://i.ibb.co/fkGnyDJ/dcw.png">
+    <meta property="og:image:type" content="image/jpeg">
+    <meta property="og:image:alt" content="noobdi">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="noobdi">
+    <meta property="og:description" content="noobdi">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="noobdi">
+    <meta name="twitter:description" content="noobdi"> 
+    <meta name="twitter:image" content="https://i.ibb.co/fkGnyDJ/dcw.png">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta property="og:site_name" content="noobdi">
+    <meta name="HandheldFriendly" content="true">
+    <meta name="keywords" content="noobdi">
+    <meta name="description" content="noobdi">
+    <title>HACKED BY CYBER GIRL</title>
+    <body>
+      <bg img="https://i.ibb.co/7gRDqsh/Picsart-24-01-24-00-22-21-844-removebg-preview.png"></bg>
+
+    <style>
+        * { margin: 0; padding: 0; }
+        body {
+          background color:#000;  ;overflow: hidden; }
+        canvas {
+            position: fixed;
+            top: 0;
+            left: 0;
+            z-index: -1;
+        }
+        .content {
+            text-align: center;
+            color: white;
+            padding: 20px;
+        }
+    </style>
+</head>
+<body>
+    <canvas></canvas>
+    <div class="content">
+        <br>
+        <img src="https://i.ibb.co/7gRDqsh/Picsart-24-01-24-00-22-21-844-removebg-preview.png" width="auto" height="400">
+<style> img{ animation-name: rotate ; animation-duration: 6s; animation-play-state: running; animation-timing-function: linear; animation-iteration-count: infinite; } @keyframes rotate{ 10% {transform:rotateY(36deg)} 20% {transform:rotateY(72deg)} 30% {transform:rotateY(108deg)} 40% {transform:rotateY(144deg)} 50% {transform:rotateY(180deg)} 60% {transform:rotateY(216deg)} 70% {transform:rotateY(252deg)} 80% {transform:rotateY(288deg)} 90% {transform:rotateY(324deg)} 100% {transform:rotateY(360deg)} } 
+    </style>
+        <br><br>
+        <center>
+            <h2>
+            <font size="10" font="" face="New Rocker" color="orange"></font>
+             <font color="orange"size="9">HACKED</font>  <font color="white"size="9">BY</font>
+              <font color=lime size="10" style="text-shadow: 0px 3px 12px green;">CYBER GIRL</font></font></h2><br>
+        </center>
+    </div>
+    <center>
+        <h2><font size="5" style="font-family: Lodger" color="red">Hello Admin You Have Been Hacked!</font></h2></center><br>
+    <center>
+        <h2>
+
+            
+        <font color="orange" style="font-family: Lodger">Hello Dear Government, Some Child Of Your Country Has Touched Our Indian Cyber Space, We Will Take Revenge, </font><br><font style="font-family: Lodger" color="white"><br><font style="font-family: Lodger" color="lime"><br> We Indians, We Are Leading Whole World, Don't Mess With Indians.</font></font> 
+        </h2>
+        </center>
+        <br><br>
+        <center>
+            <b><h3><font size="5px" color="lime" ><font color="red">|•| </font>GREETZ TO<font color="red"> |•|</font></font></h3></b>
+            <br>
+            <p>
+              <font face="courier new" color="Red" size="5px">..::<marquee scrollamount="10" direction="left" width="50%"><b>Dark Cyber Warrior | Cyber-D | New World Hacktivist Security | Indian Cyber Sanatani | Indian Cyber Force | Team UCC</b></marquee>::..
+            </p>
+        </center>
+        <br><br>
+    <center>
+    <a style="text-decoration:none;" target="_blank" href="https://i.ibb.co/7gRDqsh/Picsart-24-01-24-00-22-21-844-removebg-preview.png" rel="noopener noreferrer"><font face="Iceland" size="5" color="#FFFFFF"> [ </font>
+        <font face="Iceland" size="5" color="red">Contact With Dark Cyber Warrior</font><font face="Iceland" size="5" color="#FFFFFF">]</font></a></center>
+       <center><br><font size="2" font="" face="Montserrat" color="white">© Copyright 2023 | Dark Cyber Warrior</font>
+ <style> .memeck { position: absolute; margin: auto; height: 50%; top: 0; bottom: 0; left: 0; right: 0; } </style> <body onclick="playAudio();" class=""> <audio id="sec" __idm_id__="823202817"> <source src="https://b.top4top.io/m_2953pcym28.mp3"type="audio/mpeg"> </audio><div class="memeck"> 
+
+    <script>
+
+
+var canvas = document.querySelector('canvas'),
+    ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var letters = 'ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ';
+letters = letters.split('');
+
+var fontSize = 10,
+    columns = Math.floor(canvas.width / fontSize);
+
+var drops = [];
+for (var i = 0; i < columns; i++) {
+    drops[i] = 1;
+}
+
+function draw() {
+    ctx.fillStyle = 'rgba(0, 0, 0, .1)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = '#0f0';
+    ctx.font = fontSize + 'px monospace';
+    
+    for (var i = 0; i < drops.length; i++) {
+        var text = letters[Math.floor(Math.random() * letters.length)];
+        ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+        drops[i]++;
+        if (drops[i] * fontSize > canvas.height && Math.random() > .95) {
+            drops[i] = 0;
+        }
+    }
+}
+setInterval(draw, 33);
+
+var x = document.getElementById("sec"); function playAudio() { x.play(); } function myFunction() { var element = document.body; element.classList.toggle("dark-mode"); } function changeImage() { if (document.getElementById("imgClickAndChange").src == "body") { document.getElementById("imgClickAndChange").src = "body"; } else { document.getElementById("imgClickAndChange").src = "body"; } } 
+    </script>
+    <script type="text/javascript" src="https://dk1285.github.io/hackit/jet-planes/doodle.js"></script>
+    <script type="text/javascript">
+        doodle.init("http://dk1285.github.io/hackit/jet-planes/jet.png");
+    </script>
+</body>
+</html>
